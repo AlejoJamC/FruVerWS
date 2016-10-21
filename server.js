b3a5636fd51450a94f8d1ed45b7b454f87b6f41d
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-10-21 18:42:01 -0500

    server required packages

---
+++ server.js
@@ -0,0 +1,31 @@
+/**
+ * FruVer web service.
+ *
+ * Copyright (c) 2016-present, Alejandro Mantilla <@AlejoJamC>.
+ * MIT License.
+ */
+
+/**
+ * Module dependencies.
+ */
+
+var express             = require('express'),
+    bodyParser          = require('body-parser'),
+    favicon             = require('serve-favicon'),
+    jade                = require('jade'),
+    methodOverride      = require('method-override'),
+    moment              = require('moment'),
+    path                = require('path'),
+    session             = require('express-session'),
+    passport            = require('passport'),
+
+    logger              = require('./config/logger').Logger,
+    morgan              = require('morgan'),
+
+    routes              = require('./routes/routes'),
+
+    environment         = 'devLocal',
+    //config              = require('./config/environment.json')[environment],
+    //port                = config.port,
+
+    mongoDB             = require('./config/mongodb');
